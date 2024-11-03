fc1756b0793913876ca47a29cf687e8df7e2d1db
Tinker <mergen91@gmail.com>
2024-11-03 15:57:39 +0200

    initial commit

---
+++ src/main/java/com/kneedrop/kneedropbackend/KneedropBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kneedrop.kneedropbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KneedropBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KneedropBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kneedrop/kneedropbackend/KneedropBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kneedrop.kneedropbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KneedropBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
